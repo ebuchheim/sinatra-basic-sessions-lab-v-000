5152daa61389ac32691d8544de5aa794b0b34fb1
ebuchheim <emma.buchheim@gmail.com>
2019-04-16 08:42:43 -0700

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
+configure do
+    enable :sessions
+    set :session_secret, "2jkl38ch39sjsbi287gd63eq93w717syq91sa"
+end
+
 class App < Sinatra::Base
+
+    get '/' do
+        erb :index
+    end
+
+    post '/checkout' do
+        session[:item] = params[:item]
+        @session = session
+        erb :purchase
+    end
+
 end
